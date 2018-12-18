d9f1cd91b3047fc7e70fdfa3966930f15c22f87e
spilmont <43751712+spilmont@users.noreply.github.com>
2018-12-18 16:58:10 +0100

    a faire

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrateur
+ * Date: 18/12/2018
+ * Time: 15:31
+ */
+
+$eleves =[0=>["Flamant","Brian","36 ans","Fontaine les Vervins","Passion"=>["Cryptocurrency","botanique","alchimie"]],
+          1=>["Spilmont","Andre","31 ans","Wignehies","Passion"=>["jeux-video","nourriture","la conduite"]],
+          2=>["Bon","Romain","26 ans","Berlemont","Passion"=>["Football","cuisine","jeux-video"]],
+          3=>["Labaeye","Amandine","18 ans","Etroeungt","Passion"=>["Football","Netflix","jeux video"]],
+          4=>["Dohen","Goeffrey","24 ans","Marpant","Passion"=>["jeux-video","Manga","PHP"]],
+          5=>["Claise","Sophie","34 ans","Fourmies","Passion"=>["musique","litterature","langues etrangere"]],
+          6=>["Bultot","Bryan","26 ans","Fourmies","Passion"=>["filles","la nourriture","les soirées"]],
+          7=>["Bourret","Sebastien","40 ans","Glageon","Passion"=>["jeu-video","mangas","vins"]],
+          8=>["Ducamp","Maxime","30 ans","Beaudignies","Passion"=>["Fromage","Sci-fi","charcutrie"]],
+          9=>["Soudain","Florence","33 ans","Leval","Passion"=>["Japon","manga","jeux-video"]],
+          10=>["Duthoit","Arnaud","29 ans","Wignehies","Passion"=>["Lecture","Promenade","les cacahuettes"]],
+          11=>["Carpentier","Romain","25 ans","Jeumont","Passion"=>["Musique","informatique","jeux-video"]],
+          12=>["Carpentier","Benoit","39 ans","Fourmies","Passion"=>["informatique","astronomie","tricot"]],
+          13=>["Pronnier","David","33 ans","Fourmies","Passion"=>["les gateaux","les Bonbons","chocolats chauds"]],
+          14=>["Leclercq","Cyriak","27 ans","Fourmies","Passion"=>["jeux-video","manga","series"]],
+          15=>["Dorchies","Steven","23 ans","Floyon","Passion"=>["South Park","manga","jeux-video"]],
+          16=>["Meunier","Lubin","30 ans","Marcy sous marle","Passion"=>["Japon","manga","jeux-video"]],
+          17=>["Caron","Gregory","40 ans","Fourmies","Passion"=>["la vie","informatique","jeux-video"]],
+];
+
